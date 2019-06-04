63064ee0f07b41a7048919e8000ed38131d3c6cd
Raphael Lautenbacher <datingel@Raphaels-MacBook-Pro.local>
2019-06-04 15:26:59 +0200

    adding of the SignUp component

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import SignUp from './SignUp.js';
 
-function App() {
+class App extends Component {
+  render() {
   return (
     <div>
-      <h1>Homers Oddysee</h1>
+      <SignUp /> 
     </div>
-  );
+  )
+}
 }
 
 export default App;
---
+++ front/src/SignUp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SignUp = () => {
+  return (
+    <div>
+      <h1>Register Here</h1>
+      <input type='email' name='email' placeholder='username@email.com' />
+    </div>
+  );
+};
+
+export default SignUp;
