5ecea392f80fbf6a7a8af4c70ce7a029c2c4cb19
Raphael Lautenbacher <datingel@Raphaels-MacBook-Pro.local>
2019-06-24 20:15:45 +0200

    added the signup path

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const myRouter = express.Router();
+const connection = require('../../helpers/db');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
 
 myRouter.post('/signup', function(req, res, next) {
-  res.send('I am in POST signup');
+  const args = {
+    email: req.body.email,
+    password: req.body.password,
+    name: req.body.name,
+    lastname: req.body.lastname,
+  };
+  connection.query('INSERT INTO users SET ?', args, function(error, results, fields) {
+    if (error) res.status(500).send('Something went wrong!');
+    else res.status(200).send('Registration Successful');
+  });
 });
 
 module.exports = myRouter;
